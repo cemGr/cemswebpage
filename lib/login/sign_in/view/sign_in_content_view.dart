d09e9cf7ff03e5a86d9b9790195c7fa2bb2cbf71
cemGr <66869029+cemGr@users.noreply.github.com>
2022-08-25 11:27:39 +0200

    init Sign in Page

---
+++ lib/login/sign_in/view/sign_in_content_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  const SignInPage();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Text("Login"),
+    );
+    throw UnimplementedError();
+  }
+}
